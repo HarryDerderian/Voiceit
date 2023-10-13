ec3ececf89f7d97feccd437f73874785f6defa69
HarryDerderian <hderderian@student.cccd.edu>
2023-10-12 17:08:52 -0700

    super basic setup for home and login pages

--- base/views.py
+++ base/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
-
+from django.urls import path
+from django.http import HttpResponse
 # Create your views here.
+
+
+def home(request) :
+    return render(request, 'base/home.html')
+
+def loginPage(request) :
+    context = {}
+    return render(request, 'base/login_register.html', context)
