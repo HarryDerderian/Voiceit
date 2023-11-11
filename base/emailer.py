44f9ebbcd41e065635054f3996fd371fa36cb9c0
Harry <harryderderian@csu.fullerton.edu>
2023-11-10 21:57:07 -0800

    I added multi-threading to handle email sending (website no longer gets held up)

--- base/emailer.py
+++ base/emailer.py
@@ -1,7 +1,22 @@
 from django.core.mail import send_mail
 from django.conf.global_settings import EMAIL_HOST_USER
+import threading
+
 
 def welcome_email(user, users_email) :
+    # Create a new thread and pass the email parameters to the function
+    # This stop the website from being hung up on on sending emails.
+    thread = threading.Thread(
+        target=__welcome_email,
+        args=(user, users_email)
+    )
+    
+    # Start the thread
+    thread.start()
+
+
+
+def __welcome_email(user, users_email) :
         # Set the subject for the welcome email
         email_subject = "Welcome to Voicelt!"
         # Compose the welcome message with user-specific information
@@ -17,7 +32,22 @@ def welcome_email(user, users_email) :
         # Use the django send_mail function to send the welcome email
         send_mail(email_subject, welcome_message, EMAIL_HOST_USER, [users_email], fail_silently = False)
 
+
+
 def petition_created_email(user, users_email, petition_title) :
+    # Create a new thread and pass the email parameters to the function
+    # This stop the website from being hung on up on sending emails.
+    thread = threading.Thread(
+        target=__petition_created_email__,
+        args=(user, users_email, petition_title)
+    )
+    
+    # Start the thread
+    thread.start()
+
+
+
+def __petition_created_email__(user, users_email, petition_title) :
         # Set the subject for the welcome email
         email_subject = "Your Voicelt Petition Has Been Created"
         # Compose the welcome message with user-specific information
